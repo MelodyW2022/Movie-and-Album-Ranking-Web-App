96a0031fdb0f9be9aa02d53fece11d2105ffa5b8
Sheung Wan Wong <melodywonghxy@hotmail.com>
2023-07-15 23:45:15 -0700

    git create RankItems.js

---
+++ RankingApp/ClientApp/src/components/RankItems.js
@@ -0,0 +1,30 @@
+﻿import React, { useState, useEffect } from 'react';
+
+const RankItems = () => {
+
+    const [items, setItems] = useState([]);
+    const dataType = 1;
+
+    useEffect(() => {
+        fetch(`item/${dataType}`)
+            .then((response) => response.json())
+            .then(data => {
+                setItems(data);
+            .catch((error) => {
+                    console.error('Error fetching items:', error);
+                });
+    }, []);
+
+    return (
+        <main>
+                {items !== null ? (
+                    items.map((item) => <h3 key={item.Id}>{item.Title}</h3>)
+                ) : (
+                    <div>Loading...</div>
+                )}
+        </main>
+
+    );
+};
+
+export default RankItems;
